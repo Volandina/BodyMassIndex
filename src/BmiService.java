c2b8082eacd919d2458bfe5afe9678c12a34c2ad
Елена Кливер <volandmart@mail.ru>
2022-09-17 10:58:23 +0500

    variables corrected and refCode

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double  calculate (double weigh, double grow) {
-        double  result;
-        double c = grow/100;
-        result = weigh / (c*c) ;
+    public double calculate(double weigh, double grow) {
+        double result;
+        double heightInMeters = grow / 100;
+        result = weigh / (heightInMeters * heightInMeters);
         return result;
     }
 }
